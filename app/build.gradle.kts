4851457a2a63310ce91398dee83d100ce7a1a17b
Don Turner <donturner@google.com>
2025-05-01 15:45:12 +0100

    Add Hello World recipe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.androidx.navigation3)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/nav3recipes/basic/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.nav3recipes.basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation3.NavEntry
+import androidx.navigation3.SceneNavDisplay
+
+data object Home
+data class Product(val id: String)
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Scaffold { paddingValues ->
+
+                val backStack = remember { mutableStateListOf<Any>(Home) }
+
+                SceneNavDisplay(
+                    backStack = backStack,
+                    modifier = Modifier.padding(paddingValues),
+                    onBack = { backStack.removeLastOrNull() },
+                    entryProvider = { key ->
+                        when (key) {
+                            is Home -> NavEntry(key) {
+                                Column {
+                                    Text("Welcome to Nav3")
+                                    Button(onClick = {
+                                        backStack.add(Product("123"))
+                                    }) {
+                                        Text("Click to navigate")
+                                    }
+                                }
+                            }
+                            is Product -> NavEntry(key) {
+                                Text("Product ${key.id} ")
+                            }
+                            else -> NavEntry(Unit) { Text("Unknown route") }
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
